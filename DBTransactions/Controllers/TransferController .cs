d7c3383cd6e233bd90eab658a7d60d9cac4c3603
Amiel Cohen <amielc1@gmail.com>
2024-07-31 22:58:11 +0300

    add applicative transaction

--- DBTransactions/Controllers/TransferController .cs
+++ DBTransactions/Controllers/TransferController .cs
@@ -14,7 +14,7 @@ public class TransferController : ControllerBase
     }
 
     [HttpPost]
-    public async Task<IActionResult> Transfer([FromBody] TransferRequest request)
+    public async Task<IActionResult> TransactionTransfer([FromBody] TransferRequest request)
     {
         using (var transaction = await _context.Database.BeginTransactionAsync())
         {
@@ -41,6 +41,51 @@ public class TransferController : ControllerBase
                 await transaction.RollbackAsync();
                 throw;
             }
+        }
+    }
+
+    public async Task<IActionResult> Transfer([FromBody] TransferRequest request)
+    {
+        try
+        {
+
+            var accountA = _context.Accounts.Single(a => a.AccountId == "A");
+            var accountB = _context.Accounts.Single(b => b.AccountId == "B");
+
+            if (accountA.PendingUpdate == null && accountB.PendingUpdate == null)
+            {
+                accountA.PendingBalance = accountA.Balance - 100;
+                accountA.PendingUpdate = DateTime.Now;
+
+                accountB.PendingBalance = accountB.Balance + 100;
+                accountB.PendingUpdate = DateTime.Now;
+
+                _context.SaveChanges();
+            }
+
+            var accountsToUpdate = _context.Accounts
+               .Where(a => a.PendingUpdate != null &&
+                           (a.AccountId == "A" || a.AccountId == "B"))
+               .ToList();
+
+            foreach (var account in accountsToUpdate)
+            {
+                account.Balance = account.PendingBalance.Value;
+                account.LastUpdate = account.PendingUpdate.Value;
+                account.PendingBalance = null;
+                account.PendingUpdate = null;
+            }
+
+            _context.SaveChanges();
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            // Handle exception and rollback changes if needed
+            // Since we are not using a database transaction, ensure atomicity at the application level
+            // You may need to implement compensation logic here
+            // For example, if updating accountA succeeds but updating accountB fails, revert changes to accountA
+            throw new Exception("Transaction failed. Rolling back changes.", ex);
         }
     }
 }
