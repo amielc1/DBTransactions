a9d66d45a1293bea431a0be35115d8de63752353
Amiel Cohen <amielc1@gmail.com>
2024-08-05 17:08:16 +0300

    Add UseInMemoryDatabase

--- DBTransactions/Program.cs
+++ DBTransactions/Program.cs
@@ -8,8 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<BankingContext>(
-    options=> options.UseSqlServer("Server=sqlserver;Database=BankingDB;User Id=sa;Password=YourStrong@Passw0rd;TrustServerCertificate=True;")
-    //options => options.UseInMemoryDatabase("")
+    options => options.UseInMemoryDatabase(nameof(BankingContext))
     );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
